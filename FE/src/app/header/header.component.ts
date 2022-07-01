a6d2dc991fa93612487eb46ba0f0586fdb5714cc
yoavmil <yoavmil@gmail.com>
2022-07-01 14:10:21 +0300

    WIP header mochup

---
+++ FE/src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../Services/user.service';
+import { DeviceDetectorService } from 'ngx-device-detector';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(
+    private userService: UserService,
+    private deviceService: DeviceDetectorService) { }
+
+  ngOnInit(): void {
+  }
+
+  public hasUser(): boolean { return this.userService.hasUser; }
+  public isMobile(): boolean { return this.deviceService.isMobile(); }
+}
