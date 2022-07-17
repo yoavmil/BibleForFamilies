6fbda973a152be455fb864efe9e7db913842e876
yoavm <yoavm@MagnusMetal.com>
2022-07-17 17:23:39 +0300

    books tree

---
+++ FE/src/app/landing-page/books-tree/books-tree.component.ts
@@ -0,0 +1,92 @@
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { Component } from '@angular/core';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from '@angular/material/tree';
+
+interface BookNode {
+  name: string;
+  children?: BookNode[];
+}
+interface FlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+@Component({
+  selector: 'app-books-tree',
+  templateUrl: './books-tree.component.html',
+  styleUrls: ['./books-tree.component.css'],
+})
+export class BooksTreeComponent {
+  bookData: BookNode[] = [
+    {
+      name: 'יהושע',
+    },
+    {
+      name: 'שופטים',
+      children: [
+        {
+          name: 'מגילת רות',
+        },
+      ],
+    },
+    {
+      name: 'שמואל א+ב',
+      children: [{ name: 'תהלים' }],
+    },
+    {
+      name: 'מלכים א+ב',
+      children: [{ name: 'ישעיה' }],
+    },
+    { name: 'ירמיה' },
+    { name: 'יחזקאל' },
+    { name: 'דניאל' },
+    {
+      name: 'שיבת ציון',
+      children: [
+        {
+          name: 'עזרא',
+        },
+        {
+          name: 'נחמיה',
+        },
+        {
+          name: 'חגי',
+        },
+        {
+          name: 'זכריה',
+        },
+        {
+          name: 'מלאכי',
+        },
+      ],
+    },
+  ];
+
+  constructor() {
+    this.dataSource.data = this.bookData;
+  }
+  private _transformer = (node: BookNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+  treeControl = new FlatTreeControl<FlatNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+  hasChild = (_: number, node: FlatNode) => node.expandable;
+}
--- FE/src/app/landing-page/landing-page.module.ts
+++ FE/src/app/landing-page/landing-page.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+
 import { MainComponent } from './main/main.component';
 import { IntroComponent } from './intro/intro.component';
 import { GoalComponent } from './goal/goal.component';
-import { MatCardModule } from '@angular/material/card';
 import { GoalsComponent } from './goals/goals.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { BooksTreeComponent } from './books-tree/books-tree.component';
 
 @NgModule({
-  declarations: [MainComponent, IntroComponent, GoalComponent, GoalsComponent],
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatProgressBarModule],
+  declarations: [
+    MainComponent,
+    IntroComponent,
+    GoalComponent,
+    GoalsComponent,
+    BooksTreeComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatTreeModule,
+    MatIconModule,
+  ],
   exports: [MainComponent],
 })
 export class LandingPageModule {}
