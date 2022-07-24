e2dd0b2e9f7f265936fb57cde651fc100b6a41a5
yoavmil <yoavmil@gmail.com>
2022-07-24 23:13:46 +0300

    routes

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { HeaderComponent } from './header/header.component';
 import { BooksComponent } from './books/books.component';
 import { BuildingBlocksModule } from './building-blocks/building-blocks.module';
 import { LandingPageModule } from './landing-page/landing-page.module';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './landing-page/main/main.component';
+
+const appRoutes: Routes = [
+  { path: "", component: MainComponent }
+];
 
 @NgModule({
   declarations: [
@@ -23,7 +29,8 @@ import { LandingPageModule } from './landing-page/landing-page.module';
     BrowserAnimationsModule,
     MatToolbarModule,
     BuildingBlocksModule,
-    LandingPageModule
+    LandingPageModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
