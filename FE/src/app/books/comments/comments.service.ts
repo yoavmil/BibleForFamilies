d6a5390ada8a023b4be4ecb5ed7b7adfae1f17d2
yoavmil <yoavmil@gmail.com>
2022-08-02 19:05:57 +0300

    FE comment service WIP

--- FE/src/app/books/comments/comments.service.ts
+++ FE/src/app/books/comments/comments.service.ts
@@ -1,24 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Subject } from 'rxjs';
+import { CommentDto } from '../../../../../DTOs/comment.DTO'
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
+  private readonly commentsURL: string = "http://localhost:3001/comment";
+  private comments: CommentDto[] = [];
+  private commentsUpdated = new Subject<CommentDto[]>();
+  public get commentsListener() { return this.commentsUpdated.asObservable(); }
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) { }
 
-    this.http.get("http://localhost:3001/comment").subscribe({
-    //   next: (comments) => {
-    //     console.log(`got response ${comments}`);
-    //     console.dir(comments);
-    //   },
-    //   error: (err: HttpErrorResponse) => {
-    //     console.error(`got error ${err.message}`);
-    //     console.dir(err);
-    //   }
-    // }
-    // );
-   }
+  public getComments(url: string) {
+    this.http.get<CommentDto[]>(this.commentsURL).subscribe({
+      next: (comments) => {
+        this.comments = comments;
+        this.commentsUpdated.next(this.comments);
+      }
+    });
+  }
+
+  public addComment(comment: CommentDto) {
+    if (!comment.date) comment.date = new Date();
+    this.http.post(this.commentsURL, comment).subscribe({
+      next: (comment) => {
+        this.comments.push(comment as CommentDto);
+        this.commentsUpdated.next(this.comments);
+      }
+    });
+  }
 
 }
