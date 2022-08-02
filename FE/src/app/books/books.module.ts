a28f870e1212bcf36f5592fd704565b5481acd34
yoavmil <yoavmil@gmail.com>
2022-08-02 18:37:09 +0300

    client-server connection

--- FE/src/app/books/books.module.ts
+++ FE/src/app/books/books.module.ts
@@ -4,16 +4,20 @@ import { YehoshuaComponent } from './yehoshua/yehoshua.component';
 import { PageComponent } from './page/page.component';
 import { MatCardModule } from '@angular/material/card';
 import { GeneralComponent } from './general/general.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
     YehoshuaComponent,
     PageComponent,
     GeneralComponent,
+    CommentsComponent,
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    HttpClientModule,
   ],
 })
 export class BooksModule { }
