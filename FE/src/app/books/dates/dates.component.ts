5c6606014b19ea88582603551a7a6947ba4c158d
yoavmil <yoavmil@gmail.com>
2022-09-28 22:18:06 +0300

    dates.cont

--- FE/src/app/books/dates/dates.component.ts
+++ FE/src/app/books/dates/dates.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import data from './data.json';
+import labels from './labels.json';
 
 @Component({
   selector: 'app-dates',
@@ -9,18 +10,19 @@ import data from './data.json';
 export class DatesComponent implements OnInit {
   data: any = data;
   cycle: any[] = [];
-  crhono: any[] = [];
+  chrono: any[] = [];
   cycleCols: string[] = ['cycle', 'source', 'sum'];
-  chronoCols: string[] = ['cycle', 'year', 'source', 'sum'];
-
+  chronoCols: string[] = ['date', 'source', 'sum'];
+  public labels = labels;
   constructor() {
     this.buildCycleData();
     this.buildCrhonoData();
   }
   private buildCrhonoData() {
     for (let id of this.data.chrono) {
-      const entry = this.data.dates.find((elem: any) => elem.id == id);
-      this.crhono.push(entry);
+      let entry = this.data.dates.find((elem: any) => elem.id == id);
+      entry.date = [entry.cycle, entry.year].join(', ');
+      this.chrono.push(entry);
     }
   }
   private buildCycleData() {
